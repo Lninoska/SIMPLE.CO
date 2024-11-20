2d0d9ec59d6c44b8fcd7c2c1fc85710323cb3b8c
Lorena <lorena.n.barrioz@gmail.com>
2024-11-19 22:34:19 -0300

    segunos cambios

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,31 @@
-$(function() {
-    $("#descuento").keydown(function () {
-        $("input").css("background-color", "lightblue")
+$(document).ready(function () {
+    const descuento1 = "JQUERY2222"
+
+    $("#input").on("input", function() {
+        const frase = $(this).val();
+        if (frase === descuento1){
+            $(this).css("background-color", "lightblue")
+            $(this).css("color", "blue")
+        }
+    });
+    $("#input").on("blur", function() {
+        const frase = $(this).val();
+        if (frase === descuento1){
+            $(this).css("background-color", "lightgreen")
+            $(this).css("color", "green")
+        }
+    });
+    $("#boton").click(function () {
+        $(this).text("¿Estas seguro?")
+        $(this).css("background-color", "#FFBA14")
+    })
+    $("#boton").dblclick(function () {
+        $(this).text("¿Estas seguro?")
+        $(this).css("background-color", "#252440")
+    });
+    $("#boton").mouseout(function () {
+        $(this).css("background-color", "lightgray")
+        $(this).text("COMPRADO")
+        $(this).css("color", "green")
     })
 })
